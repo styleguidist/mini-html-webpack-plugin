1ac7e8e0e6050f04c4ceca83ed7c107bd991a7da
Artem Sapegin <artem@sapegin.ru>
2018-03-30 12:33:21 +0200

    Test: Fix tests on Node 6

--- test.js
+++ test.js
@@ -10,36 +10,43 @@ const getConfig = (options, config = {}) =>
 		config
 	);
 
-test('default options', async () => {
-	const result = await compiler({}, getConfig());
-	expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+test('default options', () => {
+	return compiler({}, getConfig()).then(result => {
+		expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+	});
 });
 
-test('custom title', async () => {
-	const result = await compiler({}, getConfig({ context: { title: 'Pizza' } }));
-	expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+test('custom title', () => {
+	return compiler({}, getConfig({ context: { title: 'Pizza' } })).then(
+		result => {
+			expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+		}
+	);
 });
 
-test('custom template', async () => {
-	const result = await compiler(
+test('custom template', () => {
+	return compiler(
 		{},
 		getConfig({
 			context: { title: 'Pizza' },
 			template: ({ title }) => `<div>${title}</div>`,
 		})
-	);
-	expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+	).then(result => {
+		expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+	});
 });
 
-test('custom filename', async () => {
+test('custom filename', () => {
 	const filename = 'pizza.html';
-	const result = await compiler({}, getConfig({ filename }));
-	expect(result.compilation.assets[filename]._value).toMatchSnapshot();
+	return compiler({}, getConfig({ filename })).then(result => {
+		expect(result.compilation.assets[filename]._value).toMatchSnapshot();
+	});
 });
 
 // TODO: https://github.com/webpack-contrib/test-utils/issues/2
-/* test('custom publicPath', async () => {
+/* test('custom publicPath', () => {
 	const publicPath = '/pizza/';
-	const result = await compiler({}, getConfig({}, { output: { publicPath } }));
-	expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+	return compiler({}, getConfig({}, { output: { publicPath } })).then(result => {
+		expect(result.compilation.assets['index.html']._value).toMatchSnapshot();
+	});
 }); */
